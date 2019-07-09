8b3b0dc7892c84198bd347d39207ee1f0f42e84e
Dmitriy Fokin <FokinDS1@gmail.com>
2019-07-09 14:15:34 +0300

    added secyrity confoguration

---
+++ src/main/java/ru/fds/tavrzcms3/Configuration/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package ru.fds.tavrzcms3.Configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.fds.tavrzcms3.Service.AppUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AppUserDetailsService appUserDetailsService;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                //.and()
+                .authorizeRequests().antMatchers("/public").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/authenticated", "/success").authenticated()
+                .and()
+                .authorizeRequests().antMatchers("/edit").hasAnyRole("USER", "ADMIN", "GUEST")
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutUrl("/logout");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return charSequence.toString().equals(s);
+            }
+        };
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+}
