e03c356e8eabfdd79e10861e288e1f8dd4120a71
dmtriy <fokinds1@gmail.com>
2019-12-04 12:52:05 +0300

    change client to clientDto. Added class ValidatorEntity

---
+++ src/main/java/ru/fds/tavrzcms3/validate/ValidatorEntity.java
@@ -0,0 +1,35 @@
+package ru.fds.tavrzcms3.validate;
+
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+
+@Component
+public class ValidatorEntity {
+
+    private String errorMessage;
+
+    public <T>Set<ConstraintViolation<T>> validateEntity(T entity){
+        Validator validator = Validation.buildDefaultValidatorFactory().usingContext().getValidator();
+        Set<ConstraintViolation<T>> violations;
+        violations = validator.validate(entity);
+
+        if(!violations.isEmpty()){
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("Ошибка валидации: ");
+            for (ConstraintViolation<T> c : violations)
+                stringBuilder.append(c.getMessage() + "(" + c.getInvalidValue() + ") в поле: \"" + c.getPropertyPath() + "\". ");
+
+            errorMessage = stringBuilder.toString();
+        }
+
+        return violations;
+    }
+
+    public String getErrorMessage(){
+        return errorMessage;
+    }
+}
